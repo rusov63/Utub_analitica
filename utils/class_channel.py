98a5e8c0cfe12b25e93ca7b72b7a6b435d874f3e
rusov63 <rusov63@yahoo.com>
2023-02-28 22:47:42 +0400

    Домашнее задание №3 14.1

--- utils/class_channel.py
+++ utils/class_channel.py
@@ -19,7 +19,7 @@ class Channel():
 
     @classmethod
     def get_service(cls):
-        api_key: str = os.getenv('API_KEY')
+        api_key: str = os.getenv('API-KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
 
@@ -33,12 +33,29 @@ class Channel():
         with open(f'{file_name}', 'w', encoding='windows-1251') as file:
             channel = json.dumps(channel_dict, indent=2, ensure_ascii=False)
             file.write(channel)
+    def __str__(self) -> str:
+        return f'Youtube-канал: {self.name}'
+
+    def __lt__(self, other) -> int:
+        """Метод сравнение каналов ">" на количество подписчиков"""
+        return self.subscriber_count > other.subscriber_count
+
+    def __gt__(self, other) -> int:
+        """Метод сравнение каналов "<" на количество подписчиков"""
+        return self.subscriber_count < other.subscriber_count
+
+    def __add__(self, other) -> int:
+        """Метод сложение два канала на количество подписчиков"""
+        return int(self.subscriber_count) + int(other.subscriber_count)
 
 id1 = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
 id2 = 'UC1eFXmJNkjITxPFWTy6RsWg'
+
 ch1 = Channel(id1)
 ch2 = Channel(id2)
-print(ch1)
-print(ch2)
-print(ch1 + ch2)
-print(ch1 < ch2)
+print(ch1) #Youtube-канал: вДудь
+print(ch2) #Youtube-канал: Редакция
+print(ch1 > ch2) #True
+print(ch1 < ch2) #False
+print(ch1 + ch2) #14000000
+
